8e91d78ce19f3710c99e688edb2d4fcaf78ca08a
Jacky Ten <yuehan1997@gmail.com>
2016-11-09 14:41:08 +0800

    Add a fruit and appear in random location

---
+++ bin/Debug/src/Fruit.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using SwinGameSDK;
+
+namespace MyGame
+{
+	public class Fruit
+	{
+		//Declare the necessary variable and const
+		private Color _color;
+		private float _x;
+		private float _y;
+		private int _width;
+		private int _height;
+
+		private const int TileWidth = 25;
+		private const int TileHeight = 25;
+
+		public Fruit ()
+		{
+			_color = Color.Green;
+		}
+
+
+		public Color Color
+		{
+			get{return _color;}
+			set{_color = value;}
+
+		}
+
+		public float X
+		{
+			get{return _x;}
+			set{_x = value;}
+
+		}
+
+		public float  Y
+		{
+			get{return _y;}
+			set{_y = value;}
+
+		}
+
+		public int width{
+			get{ return _width; }
+			set{ _width = value; }
+		}
+
+		public int height{
+			get{ return _height; }
+			set{ _height = value; }
+		}
+
+		//Methods of random generation the location of fruit
+		public void GenerateRan()
+		{
+			Random rnd = new Random();
+			_x = (rnd.Next (0, 32));
+			_y = (rnd.Next (0, 24));
+		}
+
+		//Methods of Drawing the fruit
+		public void Draw()
+		{
+			SwinGame.FillRectangle (_color, _x*TileWidth, _y*TileHeight,TileHeight, TileWidth);
+		}
+
+
+
+
+	}
+}
+
--- src/GameMain.cs
+++ src/GameMain.cs
@@ -3,36 +3,44 @@ using SwinGameSDK;
 
 namespace MyGame
 {
-    public class GameMain
-    {
-        public static void Main()
-        {
+	public class GameMain
+	{
+		public static void Main()
+		{
+			//Declare local variable of fruit and assign new fruit
+			Fruit Sfruit = new Fruit ();
 
-            //Open the game window
-            SwinGame.OpenGraphicsWindow("GameMain", 800, 600);
-            SwinGame.ShowSwinGameSplashScreen();
-            
-            //Run the game loop
-            while(false == SwinGame.WindowCloseRequested())
-            {
-                //Fetch the next batch of UI interaction
-                SwinGame.ProcessEvents();
-                
-                //Clear the screen and draw the framerate
-                SwinGame.ClearScreen(Color.White);
+			//Open the game window
+			SwinGame.OpenGraphicsWindow("GameMain", 800, 600);
+			SwinGame.ShowSwinGameSplashScreen();
 
-                SwinGame.DrawFramerate(0,0);
+			Sfruit.GenerateRan();
+
+			//Run the game loop
+			while(false == SwinGame.WindowCloseRequested())
+			{
+				//Fetch the next batch of UI interaction
+				SwinGame.ProcessEvents();
+
+				//Clear the screen and draw the framerate
+				SwinGame.ClearScreen(Color.White);
+
+				SwinGame.DrawFramerate(0,0);
 
 				// Has to go after ClearScreen and NOT before refreshscreen
 
+				//Call fruit to draw itself
+				//Sfruit.GenerateRan();
+				SwinGame.Delay (800);
+				Sfruit.Draw ();
 
-                //Draw onto the screen
-                SwinGame.RefreshScreen(60);
+				//Draw onto the screen
+				SwinGame.RefreshScreen(60);
 
 
 
 
-            }
-        }
-    }
+			}
+		}
+	}
 }
